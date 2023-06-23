0298fe8e4c9fac6347cdccd1d35ef4c082a9c8cb
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-23 15:33:51 +0530

    some rules related to {} in React

--- src/index.js
+++ src/index.js
@@ -11,15 +11,39 @@ import  ReactDOM  from 'react-dom'
 
 
 function Greeting(){
+  const num=907;
+  const styles={
+    color:"green",
+    fontSize:'1rem'
+  }
   return <>
-    <div>
+    <div style={styles}>
       <h1>Heading</h1>
+      <p>{num}</p>
+      {/* Below statement is going to throw an error because inside {} there should  */}
+      {/* always be a value instead of a statement, Got it*/}
+      {/* <p>{let a =90;}</p> */}
+      <Greet/>
     </div>
   </>
 }
 
+const Greet = ()=><h2>Greet</h2>
 
 
 
 
-ReactDOM.render(<Greet/>,document.getElementById('root'))
+// ---------------------Below are the three methods to render the root element
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.createRoot(rootElement).render(<Greeting />);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Greeting />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(<Greeting/>,document.getElementById('root'))
