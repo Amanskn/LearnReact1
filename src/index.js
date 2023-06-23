b44ec1936cff06e9a34e59397a8b2a766c608f4f
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-23 19:13:16 +0530

    {} cannot contain object but can contain array, remember it

--- src/index.js
+++ src/index.js
@@ -3,42 +3,60 @@ import  ReactDOM  from 'react-dom'
 import './index.css'
 
 
-const img='https://m.media-amazon.com/images/I/91upCY8QpWL._AC_UY327_FMwebp_QL65_.jpg';
-const title='title 1';
-const author = 'author 1'
 
+const books=[
+  {
+    img:'https://m.media-amazon.com/images/I/91upCY8QpWL._AC_UY327_FMwebp_QL65_.jpg',
+    title:'title 1',
+    author : 'author 1'
+  },
+  {
+    img:'https://m.media-amazon.com/images/I/41Q+hoHuxvL._SX258_BO1,204,203,200_.jpg',
+    title:'title 2',
+    author : 'author 2'
+  }
+]
 
 
 
 
 
 function BookList(){
+  const ab=books.map((book,i)=>{
+          
+          return <Book img={book.img} title={book.title} author={book.author}/>
+  
+        });
   return (
     <section className='booklist'>
-      <Book price={234}>
-        <h6>H3 children</h6>
-        <h1>h1</h1>
-        <h4>h3</h4>
-        <h5>h5</h5>
-       
-        </Book>
-      <Book price={23}/>
-      <Book price={245}/>
+
+      {ab}
+      
+      {books.map((book,i)=>{
+        
+        return <Book img={book.img} title={book.title} author={book.author}/>
+
+      })}
+      {["Apple","banana"]}
+      {ab}
+      {/* <Book img={books[1].img} title={books[1].title} author={books[1].author}/>
+      <Book img={books[0].img} title={books[0].title} author={books[0].author}/>
+      <Book img={books[1].img} title={books[1].title} author={books[1].author}/>
+      <Book img={books[0].img} title={books[0].title} author={books[0].author}/> */}
+      
       
     </section>
   )
 }
 
-const Book = (props)=>{
-  console.log("This is the props",props.children)
+const Book = ({img,title,author})=>{
+  // console.log("This is the props",props.children)
   return (
     <article className='book'>
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-     
-        {props.children}
-      <p> &#8377;{props.price}/-</p>
+    
     </article>
   )
 }
